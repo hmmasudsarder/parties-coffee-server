1e89c0bdb62c84d204669fc8a2aa72fe4624bd5f
hmmasudsarder <hmmasudsarder1@gmail.com>
2024-03-03 17:43:59 +0600

    update fuconaliti added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json())
@@ -23,7 +23,56 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+    app.get('/coffee', async(req, res) => {
+      const result = await coffeeCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.put('/coffee/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const query = {upsert: true};
+      const updatedCoffee = req.body;
+      const coffee = {
+        $set: {
+          name: updatedCoffee.name,
+          quantity: updatedCoffee.quantity,
+          supplier: updatedCoffee.supplier,
+          test: updatedCoffee.test,
+          category: updatedCoffee.category,
+          details: updatedCoffee.details,
+          photo: updatedCoffee.photo
+        }
+      }
+      const result = await coffeeCollection.updateOne(filter, coffee, query);
+      res.send(result);
+    })
+
+    app.post('/coffee', async(req, res) => {
+      const item = req.body;
+      console.log(item)
+      const result = await coffeeCollection.insertOne(item)
+      res.send(result)
+    });
+
+    app.delete('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
